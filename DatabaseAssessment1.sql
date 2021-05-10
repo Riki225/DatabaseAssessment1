24771d8a618abe7414006268563d2264179d0969
RIKI CHOI <103475494@student.swin.edu.au>
2021-05-10 16:33:09 +1000

    Task 3 In-Complete

--- DatabaseAssessment1.sql
+++ DatabaseAssessment1.sql
@@ -64,3 +64,63 @@ drop table event
 drop table tour
 
 SELECT * FROM sys.tables;
+
+-- Task 3
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (1, 'North', 'Jan', 9, 2016, 200, '12/10/2015');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (2, 'North', 'Jan', 9, 2016, 200, '12/16/2015');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (1, 'North', 'Feb', 13, 2016, 225, '1/08/2016');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (2, 'North', 'Feb', 13, 2016, 125, '1/14/2016');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (3, 'North', 'Feb', 13, 2016, 225, '2/03/2016');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (1, 'South', 'Jan', 9, 2016, 200, '12/10/2015');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (2, 'South', 'Jan', 16, 2016, 200, '12/18/2015');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (3, 'South', 'Jan', 16, 2016, 200, '1/09/2016');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (2, 'West', 'Jan', 29, 2016, 225, '12/17/2015');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (3, 'West', 'Jan', 29, 2016, 200, '12/18/2015');
+INSERT INTO Booking (ClientId, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+VALUES (4, 'West', 'Jan', 29, 2016, 200, '12/18/2015');
+
+INSERT INTO Client (ClientId, Surname, GivenName, Gender)
+Values (1, 'Price', 'Taylor', 'M');
+INSERT INTO Client (ClientId, Surname, GivenName, Gender)
+Values (2, 'Gamble', 'Ellyse', 'M');
+INSERT INTO Client (ClientId, Surname, GivenName, Gender)
+Values (3, 'Tan', 'Tilly', 'M');
+INSERT INTO Client (ClientId, Surname, GivenName, Gender)
+Values (4, 'Choi', 'Riki', 'M');
+
+INSERT INTO Tour (TourName, Description)
+Values ('North', 'Tour of wineries and outlets of the Bedigo and Castlemaine region');
+INSERT INTO Tour (TourName, Description)
+Values ('South', 'Tour of wineries and outlets of Mornington Penisula');
+INSERT INTO Tour (TourName, Description)
+Values ('West', 'Tour of wineries and outlets of the Geelong and Otways region');
+
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+Values ('North', 'Jan', 9, 2016, 200);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+Values ('North', 'Feb', 13, 2016, 225);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+Values ('South', 'Jan', 9, 2016, 200);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+Values ('South', 'Jan', 16, 2016, 200);
+INSERT INTO Event (TourName, EventMonth, EventDay, EventYear, EventFee)
+Values ('West', 'Jan', 29, 2016, 225);
+
+select * from Client
+
+SELECT GivenName, Surname, TourName, Description, EventYear, EventMonth, EventDay, EventFee, Payment
+From Booking B
+INNER JOIN Client B
+ON C.ClientId = B.ClientId
+INNER JOIN Event E
+ON B.TourName = E.TourName
